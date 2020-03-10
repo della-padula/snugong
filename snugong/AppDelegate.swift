3d858bedb1b01d797e7712393c50c4edf384107a
della-padula <della.kimko@gmail.com>
2020-03-10 12:19:00 +0900

    first commit

---
+++ snugong/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  snugong
+//
+//  Created by TaeinKim on 2020/03/10.
+//  Copyright © 2020 TaeinKim. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
